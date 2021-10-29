7317fd93a682958185b4281485e11f6b350e7438
Azzam R Alrashed <azzam.rar@gmail.com>
2021-10-29 11:22:31 +0300

    worked on the LoginView

--- spark/ViewController.swift
+++ spark/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import PhoneNumberKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet var waveShapes: [UIImageView]!
+    
+    @IBOutlet weak var phoneNumberTF: PhoneNumberTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        phoneNumberTF.withFlag = true
+        phoneNumberTF.withDefaultPickerUI = true
+        phoneNumberTF.withExamplePlaceholder = true
+        waveShapes[0].tintColor = #colorLiteral(red: 1, green: 0.6761596203, blue: 0, alpha: 1)
+        waveShapes[1].tintColor = #colorLiteral(red: 1, green: 0.6761596203, blue: 0, alpha: 1)
+        waveShapes[2].tintColor = #colorLiteral(red: 1, green: 0.6593478322, blue: 0, alpha: 1)
+        waveShapes[3].tintColor = #colorLiteral(red: 1, green: 0.6593478322, blue: 0, alpha: 1)
+        UIView.animate(withDuration: 3, delay: 0, options: [.repeat,.autoreverse]) {
+            
+            for shape in self.waveShapes {
+                shape.tintColor = #colorLiteral(red: 0.998763144, green: 0.6134348512, blue: 0, alpha: 1)
+                shape.frame.origin.y += [5,10,15].shuffled().first!
+            }
+        }
+
     }
 
 
